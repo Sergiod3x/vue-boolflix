41d06691de3d48407ce63db825a11eb00effd2fa
Sergiod3x <sergiod3x.@gmail.com>
2021-07-20 22:32:25 +0200

    mailstone ok

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <!-- <Loader v-if="(isNaN(albums.page) )"/> -->
-    <!-- <button @click="searchAlbum(filteredArray)">Filtra</button> -->
-    <Nav  @search="searchAlbum"/>
+    <Nav  @search="searchVideo"/>
     <Main :films="filteredArray"/>
-
   </div>
 </template>
 
@@ -23,7 +20,6 @@ export default {
   },
   data(){
     return{
-      albums:{},
       filteredArray:[],
       filmsArray:[],
 
@@ -32,26 +28,24 @@ export default {
   created(){
     //alla creazione carico la lista di film preferiti del periodo
     axios.get("https://api.themoviedb.org/3/movie/popular?api_key=e4d8fd5e18690e57267700ebfb913c8d&language=it-US&page=1").then((result)=> {
-      this.albums = result
       this.filteredArray = result.data.results
       this.filmsArray= result.data.results
        console.log("ciao")
-      console.log(this.albums)
          console.log("ciao")
             console.log(this.filmsArray)
     })
   },
   methods:{
-    searchAlbum(searchString){
+    searchVideo(searchString){
       if(searchString.length == 0){
         this.filteredArray=  this.filmsArray;
       } else{
         let searchBottString = "https://api.themoviedb.org/3/search/multi?api_key=e4d8fd5e18690e57267700ebfb913c8d&query="+searchString;
         axios.get(searchBottString).then((result)=> {
         this.filteredArray = result.data.results
-         console.log("ciao")
-           console.log("ciao")
-              console.log(this.filteredArray)
+        //  console.log("ciao")
+        //    console.log("ciao")
+        //       console.log(this.filteredArray)
       
         })
       } 
@@ -67,12 +61,4 @@ export default {
 #app{
   background-color:$primary-color ;
 }
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
 </style>
