1eb81072b8959ec84ad16b32e17f7318938fef4d
Sergiod3x <sergiod3x.@gmail.com>
2021-07-20 21:09:32 +0200

    star ok

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -5,21 +5,22 @@
             <img :src="imgSrc(films.poster_path)" alt=""/>
             <div class="info">
                 <div class="text">
-                    <H4>Titolo :</H4>   {{films.title}} 
-                    <H4>Titolo originale:   </H4>  {{films.original_title}}
-                    <h4>Lingua originale</h4><img class="flag" :src="returnFlag(films.original_language)" alt="" />
+                   Titolo :  <H3>{{films.title}} {{films.name}}</H3>  
+                    Titolo originale:     <H3>{{films.original_title}}{{films.original_name}}</H3>
+                    Lingua originale  <img class="flag" :src="returnFlag(films.original_language)" alt="" />
                     <div class="vote">
-                        <h4>Popolarità</h4>
+                        Popolarità
                         <ul v-if="vote > 0"> 
                             <li><i class="fas fa-star gold"></i></li>
                             <li v-if="vote > 1"><i class="fas fa-star gold"></i></li>
-                            <li v-else><i class="far fa-star "></i></li>
+                            <li v-else><i class="far fa-star"></i></li>
                             <li v-if="vote > 2"><i class="fas fa-star gold"></i></li>
                             <li v-else><i class="far fa-star"></i></li>
                             <li v-if="vote > 3"><i class="fas fa-star gold"></i></li>
                             <li v-else><i class="far fa-star"></i></li>
                             <li v-if="vote > 4"><i class="fas fa-star gold"></i></li>
                             <li v-else><i class="far fa-star"></i></li>
+                            
                         </ul>
                     </div>
 
@@ -39,7 +40,7 @@ export default{
    },
    data(){
     return{
-        vote: Math.round(this.films.popularity / 2),
+      vote: Math.round(this.films.vote_average / 2),
       languageArray:["it","de","en","es","fr","pt"],
       }
     },
@@ -48,11 +49,14 @@ export default{
             return  "https://image.tmdb.org/t/p/w342"+ partialSrc;
         },
         returnFlag(Language) {
+            console.log(this.films.vote_average);
+            console.log(this.vote);
             //Se non ho la bandiera corrispondente stampo la bandiera della terra
             if(this.languageArray.includes(Language)){
                 return require("../assets/flags/" + Language + ".svg");
             }
             return require("../assets/flags/all.png");
+            
         
     }
   }
@@ -65,7 +69,7 @@ export default{
     position: relative;
     margin:25px;
     .info{
-        background-color: rgba(0, 0, 0, 0.452);
+        background-color: rgba(0, 0, 0, 0.514);
         width: 100%;
         height: 100%;
         position: absolute;
@@ -83,7 +87,6 @@ export default{
             line-height: 18px;
             ul {
                 display: flex;
-                margin-top: 5px;
                 list-style-type: none;
                 align-items: center;
                 
